119980e74048494f892253fafc7bf6a7be55e5b5
Abhinav Mishra <hey@abhi.dev>
2025-01-13 11:25:24 +0700

    fix: [ISSUE-2] fix lint issues
    Signed-off-by: Abhinav Mishra <hey@abhi.dev>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,21 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  includeIgnoreFile(gitignorePath),
+  { files: ["./src/**/*.{js,mjs,cjs,ts}"] },
+  {
+    ignores: ["release.config.js"]
+  },
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
